7688fc11525b97ff307220cb202b5a4a549087f1
acakojic <a.kojic@yahoo.com>
2018-06-21 14:25:16 +0200

    Added in SecondActivity array with items pushed to ListView, onclick one of items we display that items name in TextView

--- app/src/main/java/com/example/koja/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/koja/myapplication/SecondActivity.kt
@@ -4,13 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
 import com.example.koja.myapplication.R.id.idButton
+import com.example.koja.myapplication.R.id.idListView
 
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
+
+    val items = arrayListOf<String>("Jabuka", "Sljiva", "Malina", "Kajsija", "Visnja")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +26,6 @@ class SecondActivity : AppCompatActivity() {
 
 
         //get textString from MainComponent
-
         val intentText = getIntent()
         val textName = intentText.getStringExtra("textName")
         idTextView.text = textName
@@ -29,6 +33,15 @@ class SecondActivity : AppCompatActivity() {
         idButton.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+
+        //ListView
+        val idListView: ListView = findViewById(idListView)
+        val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        idListView.adapter = adapter
+
+        idListView.setOnItemClickListener { parent, view, position, id ->
+            idTextView.text = items[position]
         }
     }
 
